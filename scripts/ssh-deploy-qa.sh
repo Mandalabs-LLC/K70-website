6fcf8fc8f0135afe07df4679601a2749988756b7
Bikalpa Sharma <bikalpa.sharma@biworldwide.com>
2024-10-30 16:38:03 +0545

    chore: Circle ci configs

---
+++ scripts/ssh-deploy-qa.sh
@@ -0,0 +1,9 @@
+tar -xzf ~/k70/webapp.tar.gz -C ~/k70/ && rm -f ~/k70/webapp.tar.gz
+
+cd k70
+
+# build the image
+docker build -f docker/prod/Dockerfile -t k70-web-app:latest .
+docker rm -f k70-web-app || true
+docker image prune -f
+docker run -d --network frontend -e "VIRTUAL_HOST1=qa.kanchenjunga70.com" --name k70-web-app k70-web-app:latest
---
+++ scripts/ssh-setup.sh
@@ -0,0 +1,5 @@
+# Remove existing mdlabs directory and it contents
+rm -rf ~/k70/ || true
+
+# Create a new mdlabs directory
+mkdir k70
