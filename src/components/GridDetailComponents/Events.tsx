388bb8d7584008d4a6c527ecf04ebd91fd0d8850
Neha Shah <neha.shah@mandalabs.com>
2024-11-13 15:23:13 +0545

    feat: added k70events and partner page

--- src/components/GridDetailComponents/Events.tsx
+++ src/components/GridDetailComponents/Events.tsx
@@ -1,9 +1,74 @@
-export const Events = () => {
+"use client"
+import { useState } from "react";
+import Image from "next/image";
+import background2 from '../../../public/images/background2.jpg';
+import { k70Events } from '../../data/k70events';
+import Link from "next/link";
 
-    return (
-      <div>
-        This is Events
+export const Events = () => {
+  const [currentIndex, setCurrentIndex] = useState<number>(0);
+
+  return (
+    <>
+      <Image
+        src={background2}
+        alt='Hero_Section_Images'
+        className=' -z-40 fixed w-full h-screen top-[4.5rem] md:top-[12rem] left-0 object-cover'
+        loading='lazy'
+      />
+      <div className="bg-white opacity-90 w-full h-auto flex flex-col  p-4 md:px-20 md:py-[60px]">
+        <h1 className="uppercase text-blue-dark bebasNeue tracking-widest text-4xl  md:text-6xl pb-4 md:pb-6 z-10">Activities for the event in Nepal</h1>
+
+        <div className="hidden md:flex gap-[160px] my-[60px]">
+          <div className="w-1/3 flex flex-col justify-end">
+            {k70Events.map((data, index) => (
+              <div key={index} className={` flex flex-row items-center border-b border-blue-light p-3 cursor-pointer text-blue-light text-3xl ${index === currentIndex && 'text-primary text-5xl pb-5'}`}
+                onMouseEnter={() => setCurrentIndex(index)}
+              >
+                <div className={`absolute left-0 bg-black h-2 w-20 ${index === currentIndex ? 'block' : 'hidden'}`}></div>
+                <p className="uppercase bebasNeue tracking-wider">{data.date}</p>
+              </div>
+            ))}
+          </div>
+
+          <div className="w-2/3 flex flex-col justify-between">
+            <p className="text-primary-light text-base md:text-lg tracking-wider">
+              {k70Events[currentIndex].content}
+            </p>
+            <p className="uppercase text-blue-dark bebasNeue tracking-wider text-3xl text-right">
+              {k70Events[currentIndex].title}
+            </p>
+          </div>
+
+        </div>
+
+        <div className="flex flex-col md:hidden gap-6 ">
+          {k70Events.map((data, index) => (
+            <div key={index} className="border border-black space-y-5">
+              <p className="uppercase bebasNeue tracking-wider border-b border-black text-blue-light text-2xl px-5 py-3">{data.date}</p>
+              <p className="text-primary-light text-base tracking-wider px-5">
+                {data.content}
+              </p>
+              <p className="uppercase text-blue-dark bebasNeue tracking-wider text-2xl text-right px-5 pb-3">
+                {data.title}
+              </p>
+            </div>
+          ))}
+        </div>
+
       </div>
-    )
-  
-}
+
+      <div className="flex flex-col justify-center items-center bg-white p-4 md:px-20 md:py-[60px]">
+        <p className="text-primary-light text-base md:text-lg tracking-wider text-center">
+          2025 marks the 70th anniversary since the first summit to Kanchenjunga in 1955. It is a voyage of celebration that will honor a legacy inspiring many. Various events are planned to commemorate this milestone, hosted by communities in both Nepal and the United Kingdom.
+          <br /><br />
+          These celebrations will mirror the success of the Everest 70 event in 2023 <Link href="www.everest70.com" className="text-blue-light underline">(www.everest70.com)</Link>, showcasing the deep connections and shared history between the two regions. It promises to be a meaningful occasion filled with activities that highlight the enduring impact of this legacy.
+          <br /><br />
+          celebrations will honor pioneering climbers and foster community engagement while highlighting the region&apos;s rich cultural heritage.
+        </p>
+      </div>
+
+    </>
+  )
+
+}; 
---
+++ src/data/k70events.ts
@@ -0,0 +1,32 @@
+interface k70Events {
+    date: string;
+    content: string;
+    title: string;
+}
+
+export const k70Events: k70Events[] = [
+    {
+        date: '19 April, 2025',
+        content: `The celebrations will start from the heart of the Kanchenjunga region, with a special event taking place
+         in the scenic village of Lelep, Taplejung with members of the local community, KCAMC, and the local government.`,
+        title: '- Lelep, Taplejung'
+    },
+    {
+        date: '22 April, 2025',
+        content: `A celebration will be hosted by the British Ambassador to Nepal at the Ambassador’s residence in Kathmandu.`,
+        title: ' - British Ambasadoor’s Residence, Kathmandu'
+    },
+    {
+        date: '23 April, 2025',
+        content: `The Kanchenjunga 70 celebrations will include an event in Kathmandu, Nepal, uniting the private sector, 
+        media, government stakeholders, and international organizations to pay tribute to the legacy of Kanchenjunga's first ascent.`,
+        title: '- Aloft Hotel, Kathmandu'
+    },
+    {
+        date: '5 June, 2025',
+        content: `In the United Kingdom, the festivities will culminate at the prestigious Royal Geographic Society, where 
+        adventurers, mountaineers, and enthusiasts will gather to commemorate this historic moment. The celebrations will honor 
+        pioneering climbers and foster community engagement while highlighting the region's rich cultural heritage.`,
+        title: '- Royal Geographical Society, London'
+    },
+]
