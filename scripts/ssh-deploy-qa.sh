06c04c73d4326d74293eb1ab3a946be0b42d0df9
Pawan Parajuli <167839794+ppmdlabs@users.noreply.github.com>
2025-03-03 14:21:36 +0545

    chore: deployment config updates (#35)
    Co-authored-by: pawanprjl <pawanparajuli8@gmail.com>

--- scripts/ssh-deploy-qa.sh
+++ scripts/ssh-deploy-qa.sh
@@ -1,9 +1,9 @@
-tar -xzf ~/k70/webapp.tar.gz -C ~/k70/ && rm -f ~/k70/webapp.tar.gz
+tar --remove-files -xzf ~/k70/webapp.tar.gz -C ~/k70/
 
 cd k70
 
 # build the image
 docker build -f docker/prod/Dockerfile -t k70-web-app:latest .
 docker container rm -f k70-web-app || true
-docker image prune -f
-docker run -d --network frontend -e "VIRTUAL_HOST=https://qa.kanchenjunga70.com" --name k70-web-app k70-web-app:latest
+docker image prune -f --filter "until=24h"
+docker run -d --memory=512m --cpus=0.5 --restart unless-stopped --network frontend -e "VIRTUAL_HOST=https://qa.kanchenjunga70.com" --name k70-web-app k70-web-app:latest
