018b082ff5210103f60a0de9eccd4e77cd65a719
Pawan Parajuli <167839794+ppmdlabs@users.noreply.github.com>
2025-03-11 17:18:25 +0545

    chore: add deploy config to prod deployment (#39)
    Co-authored-by: pawanprjl <pawanparajuli8@gmail.com>

---
+++ scripts/ssh-deploy-prod.sh
@@ -0,0 +1,9 @@
+tar --remove-files -xzf ~/k70/webapp.tar.gz -C ~/k70/
+
+cd k70
+
+# build the image
+docker build -f docker/prod/Dockerfile -t k70-web-app:latest .
+docker container rm -f k70-web-app || true
+docker image prune -f --filter "until=24h"
+docker run -d --memory=512m --cpus=0.5 --restart unless-stopped --network frontend -e "VIRTUAL_HOST1=https://kanchenjunga70.com" -e "VIRTUAL_HOST2=https://www.kanchenjunga70.com" --name k70-web-app k70-web-app:latest
